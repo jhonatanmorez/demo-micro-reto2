5000119531e7f8fb8c7b653685dee5b9069e87b3
jhonatanmorez <jhonatan.more5@hotmail.com>
2025-10-04 16:45:35 -0500

    initial commit

---
+++ demo-micro/src/main/java/com/demo/App.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Reto 1 update - Hello World desde Docker + GitHub Actions! CICD completo Jorge More";
+    }
+}
